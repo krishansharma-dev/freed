2542f664a2b5989187213d74fdfb968d77a0571a
krishansharma-dev <krishansharma@qurilo.com>
2025-07-18 17:27:35 +0530

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from 'expo-router/stack'
+import { Tabs } from 'expo-router'
 
 export default function Layout() {
-  return <Stack />
+  return <Tabs />
 }
---
+++ app/welcome.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Welcome() {
+  return (
+    <View>
+      <Text style={{ color: 'red' , fontSize: 20,margin: 100 }}>Welcome</Text>
+    </View>
+  )
+}
