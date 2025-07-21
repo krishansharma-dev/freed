f6d32f1af19961fa00c2d46f660a823d59d1ba09
krishansharma-dev <krishansharma@qurilo.com>
2025-07-21 15:31:05 +0530

    navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,16 @@ export default function Layout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="home" size={24} color="black" />
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+        <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="search1" size={24} color={color} />
           ),
         }}
       />
@@ -18,27 +27,20 @@ export default function Layout() {
         options={{
           title: 'Notification',
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="bells" size={24} color="black" />
-    )  }}
+            <AntDesign name="bells" size={24} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="profile" size={24} color="black" />
+            <AntDesign name="profile" size={24} color={color} />
           ),
         }}
       />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="search1" size={24} color="black" />
-          ),
-        }}
-      />
+    
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,19 @@
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Slot } from "expo-router";
+
+const myTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: 'red',
+  }
+};
 
 export default function RootLayout() {
   return(
+    <ThemeProvider value={myTheme}>
     <Slot />
+    </ThemeProvider>
   )
 }
  
